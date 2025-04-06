c889eadc1ddac97c53670422d2387043513bc865
Piele Matei <99535423+mpiele@users.noreply.github.com>
2025-04-06 15:05:01 +0300

    added animated background and scrolling feature

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-document.getElementById("learnmore_button").onclick = function () {
-    location.href = "about.html"
+//document.getElementById("learnmore_button").onclick = function () {
+    //location.href = "about.html"
+//}
+
+function scrollToSection1() {
+    const section = document.getElementById("about-section");
+    section.scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollToSection2() {
+    const section = document.getElementById("contact-section");
+    section.scrollIntoView({ behavior: "smooth" });
+}
+  
